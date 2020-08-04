f3d1397629e3ed2450a1c5ea09613cebf4be27d9
Cleef Souza <a.cleef.souza@gmail.com>
2020-08-03 21:56:29 -0300

    Implementando filter

---
+++ 04-lists/for-forin/3.filter.js
@@ -0,0 +1,40 @@
+const { obterPessoas } = require('./service'); // Destruct
+
+Array.prototype.meuFilter = function (callback) {
+    const lista = [];
+
+    for (index in this) {
+        const item = this[index];
+        const result = callback(item, index, this);
+
+        if (!result) continue;
+
+        lista.push(item);
+    }
+
+    return lista;
+}
+
+async function main() {
+    try {
+        const { results } = await obterPessoas('a');
+
+        // const familiaLars = results.filter(item => {
+        //     const result = item.name.toLowerCase().indexOf('lars') !== -1;
+
+        //     return result;
+        // });
+
+        const familiaLars = results.meuFilter((item, index, lista) => {
+            return item.name.toLowerCase().indexOf('lars') !== -1;
+        });
+
+        const names = familiaLars.map(pessoa => pessoa.name);
+
+        console.log(names);
+    } catch (error) {
+        console.error('DEU RUIM', error);
+    }
+}
+
+main();
